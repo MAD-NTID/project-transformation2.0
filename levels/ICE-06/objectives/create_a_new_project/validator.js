b75f9894c4d5f5c4f6e743aa788952857e9a8943
kemoycampbell <ksc2650@rit.edu>
2023-02-07 19:18:15 -0500

    ice-6 tested

--- levels/ICE-06/objectives/create_a_new_project/validator.js
+++ levels/ICE-06/objectives/create_a_new_project/validator.js
@@ -3,10 +3,12 @@ In your validation code, you can require core Node.js modules,
 third-party modules from npm, or your own code, just like a regular
 Node.js module (since that's what this is!)
 */
+const assert = require("assert");
+const { isTwilio } = require("../lib/example_helper");
 const {exec} = require("child_process");
 const path = require('path');
 const fs = require('fs');
-const { cleanPath } = require("../../../../scripts/utils");
+const {cleanPath, readFile, normalizeLineEndings, dotnet, projectInfo} = require("../../../../scripts/utils");
 
 /*
 Objective validators export a single function, which is passed a helper
@@ -19,77 +21,68 @@ have completed the challenge as instructed.
 */
 module.exports = async function (helper) {
   // We start by getting the user input from the helper
-  const { answer1, answer2} = helper.validationFields;
+  const { answer0,answer1, answer2} = helper.validationFields;
+
+  if(!answer0 || answer0.length === 0)
+    return helper.fail("Please enter your first and last name");
+
+  let fullName = answer0.split(" ");
+  if(fullName.length < 2)
+    return helper.fail("You must enter both your first and last name separate by a space. Example:Kemoy Campbell");
+
 
   if(!answer1 || (answer1!=='Console.WriteLine' && answer1!=='Console.WriteLine()' && answer1 !== 'Console.WriteLine();'))
-    return helper.fail('Incorrect answer regarding the first question. Please review the ppt slides');
+    return helper.fail('Incorrect answer regarding the second question. Please review the ppt slides');
 
   if(!answer2)
     return helper.fail('The absolute path cannot be empty!');
 
-  let fullPath=path.join(cleanPath(answer2), 'Program.cs');
 
-  // //git bash fixer
-  // if(process.platform==='win32' && answer2.toLowerCase().includes('/c/')){
-
-  //   //disregard the /c
-  //   fullPath = path.join(answer2.substring(2),'Program.cs');
-  // } else {
-  //   fullPath = path.join(answer2,'Program.cs');
-  // }
-
-  if(!fullPath.includes("WorkingWithVisualStudioCode"))
-    return helper.fail('The name of your project doesnt appeared to be correct. Your project should be named WorkingWithVisualStudioCode');
-
-  if(!fs.existsSync(fullPath)){
-    return helper.fail('Incorrect-> Cannot find Program.cs in WorkingWithVisualStudioCode');
+  if(!answer2.endsWith("WorkingWithVisualStudioCode")){
+    return helper.fail('The name of your project doesnt appeared to be correct. Your project should be named WorkingWithVisualStudioCode and the directory path must ends with WorkingWithVisualStudioCode');
   }
 
-  fs.readFile(fullPath, 'utf8', (err, data) => {
-    if (err) {
-      return helper.fail(err);
-    }
+  try {
+    let project = await projectInfo(answer2);
 
-    if(!data.includes("Hello Nerds/Geeks!!--- I'm"))
-      return helper.fail('Incorrect information in the Console.WriteLine. Check the objective menu again');
+    //reading the file data
+    const data = project.programContent;
+    if(data.length === 0)
+      return helper.fail("Program.cs cannot be empty!");
 
-    if(data.includes("<first name>"))
+
+
+    const stdout = await dotnet(`run --project ${project.project}`, 15, "The program timed out while attempting to run your project with dotnet run");
+
+    if(!stdout.includes("Hello Nerds/Geeks!!--- I'm"))
+      return helper.fail("Hello Nerds/Geeks!!--- I'm is missing from the console! please check the objective menu and try again");
+
+
+    if(stdout.includes("<first name>"))
       return helper.fail("replace <first name> with your first name");
 
-    if(data.includes("<last name>"))
+    if(stdout.includes("<last name>"))
       return helper.fail("replace <last name> with your last name");
 
-    //attempt to ensure that the project compiled
-    const { exec } = require('child_process');
-    let project = fullPath.replace('Program.cs', '');
 
-    let command = 'dotnet';
-    //mac have the dotnet executable at a specific location and the symlink doesnt seems to be available when calling through script
-    if(process.platform==='darwin')
-      command = '/usr/local/share/dotnet/dotnet';
-    const ls = exec(`${command} run --project ${project}`, function (error, stdout, stderr) {
-      if (error) {
-        console.log(error);
-        console.log(error.stack);
-        return helper.fail('An error occurred while compiling your project')
-      } else if(stderr){
-        console.log(stderr);
-        return helper.fail('An error occurred while compiling your project')
-      } else {
-        // The way we usually write validators is to fail fast, and then if we reach
-        // the end, we know the user got all the answers right!
-        helper.success(`Hooray! You did it!`,  [{ name: "GETTING_START_PATH", value: fullPath }]);
-      }
-      console.log('Child Process STDOUT: ' + stdout);
-      console.log('Child Process STDERR: ' + stderr);
-    });
+    if(!stdout.includes(fullName[0]) || !stdout.includes(fullName[1]))
+      return helper.fail("You must include you first and last name in the console. Check the objective menu for the sample output and try again");
 
-    ls.on('exit', function (code) {
-      console.log('Child process exited with exit code ' + code);
-    });
+    helper.success(`Hooray! You did it!`,  [{ name: "GETTING_START_PATH", value: cleanPath(project.project)},{name:"FULL_NAME", value:answer0}]);
+
+  }catch(e)
+  {
+    return helper.fail(e.message);
+  }
 
 
-  });
+
+
+
+
+
+
+
 
 
 
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-const {cleanPath} = require("./scripts/utils");
+const {cleanPath, dotnet, projectInfo,} = require("./scripts/utils");
+const dotty = async function()
+{
+    
+    let project = await projectInfo('/home/cypher/Desktop/Test/Testing Space Folder/Testing Space Folder/WorkingWithVisualStudioCode');
+    let info = await dotnet(`run --project ${project.project}`, 15);
+}
 
 
-
-console.log(cleanPath("/c/Users/cyber/Desktop/Test Space Folder/Lot lot lot lot lot more space/my-first-github-classroom-kemoycampbell/HelloWorld"));
+dotty();
